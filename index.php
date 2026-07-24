0c2471fcf4623143a56d2ce06956583d900a1f09
Dominik <dominikbucak@gmail.com>
2026-07-24 03:01:45 +0200

    responsive update

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                                 h-[0.0625rem] 
                                 bg-[var(--line-color)]">
                     </div>
-                    <div class="flex flex-row md:gap-[var(--large-gap)] lg:justify-start justify-center">
+                    <div class="flex flex-row md:gap-[var(--extra-large-gap)] lg:justify-start justify-center">
                         <div class="flex flex-col">
                             <h3 class="h2Text text-center">10+</h3>
                             <p class="bodyText text-center">ROKOV PRAXE</p>
@@ -80,8 +80,8 @@
         </section>
 
         <!-- RECOMMENDED PRODUCTS -->
-        <section class="bg-[var(--decent-color)] px-4 md:px-16  lg:px-32">
-            <div class="max-w-[80rem] mx-auto grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8">
+        <section class="bg-[var(--decent-color)] px-4 md:px-16  lg:px-32 relative">
+            <div class="max-w-[80rem] mx-auto grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 py-10 blur-sm">
                 <!-- Header -->
                 <div class="col-span-4 md:col-span-8 lg:col-span-12">
                     <p class="bodyText text-[var(--accent-primary-color)] font-bold">
@@ -141,7 +141,14 @@
                     </div>
                 </div>
             </div>
+            <!-- Overlay s textom -->
+            <div class="absolute inset-0 flex items-center justify-center">
+                <p class="h2Text text-center bg-[var(--decent-color)]/70 px-8 py-4 rounded-[var(--rounded-medium)]">
+                    Na katalógu pracujeme
+                </p>
+            </div>
         </section>
+        
         <!-- SERVICES -->
         <section class="bg-[var(--secondary-color)] px-4 md:px-16  lg:px-32">
             <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 max-w-[80rem] mx-auto py-20 ">
@@ -169,6 +176,7 @@
                 </div>
             </div>
         </section>
+
         <!-- PARTNERS -->
         <section class="bg-[var(--bg-color)] lg:px-32">
             <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 max-w-[80rem] mx-auto py-20 ">
@@ -206,5 +214,122 @@
                 </div>
             </div>   
         </section>
+
+        <!-- QUESTIONS -->
+        <section class="bg-[var(--decent-color)] px-4 md:px-16  lg:px-32">
+            <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 max-w-[80rem] mx-auto py-20  items-center">
+                <div class="col-span-4 md:col-span-8 lg:col-span-12 flex flex-col items-center justify-center text-center gap-[var(--medium-gap)]">
+                    <p class="pText text-[var(--accent-primary-color)] font-[800]">NAVŠTÍVTE NAŠU PREDAJŇU</p>
+                    <h1 class="h1Text">Prídte si vybrať techniku, na ktorú sa môžete spoľahnúť</h1>
+                    <p class="bodyText mx-auto w-[80%] mt-3">V našej predajni nájdete široký výber záhradnej a lesnej techniky spolu s odborným poradenstvom. Pomôžeme vám vybrať stroj, ktorý bude spoľahlivo slúžiť a dokonale vyhovovať vašim potrebám.</p>
+                    <button class="qaButton bg-[var(--secondary-color)] text-white">Máte otázky?</button>
+                </div>
+            </div>
+        </section>
+
+        <!-- CONTACT -->
+        <section class="bg-[var(--bg-color)] px-4 md:px-16  lg:px-32">
+            <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 max-w-[80rem] mx-auto py-10 ">
+                <div class="col-span-5">
+                    <div class="flex flex-col gap-[var(--extra-small-gap)]">
+                        <p class="bodyText text-[var(--accent-primary-color)] font-bold">KONTAKT</p>
+                        <h2 class="h2Text">Navštívte nás</h2>
+                        <p class="pText mt-2">Príďte sa pozrieť, porozprávať a vybrať si správnu techniku. 
+                           Radi vám poradíme na základe skúseností z praxe a pomôžeme nájsť stroj, ktorý bude presne vyhovovať vašim potrebám. Kanvica je vždy pripravená.</p>
+                        <div class="mt-10
+                            w-full 
+                            h-[0.0625rem] 
+                            bg-[var(--line-color)]">
+                        </div>
+                        <div class="flex flex-col gap-[var(--large-gap)]">
+                            <div class="flex flex-col gap-[var(--small-gap)]">
+                                <p class="bodyText">ADRESA</p>
+                                <p class="pText">Moravská 619, 907 01 Myjava</p>
+                            </div>
+
+                            <div class="flex flex-col gap-[var(--small-gap)]">
+                                <p class="bodyText">OTVÁRACIE HODINY</p>
+                                <div class="flex flex-col gap-[var(--extra-small-gap)]">
+                                    <p class="pText"><strong>PO-PIA</strong>: 8:00 - 16:00</p>
+                                    <p class="pText"><strong>SO</strong>: 8:00 - 11:30</p>
+                                    <p class="pText"><strong>NE</strong>: ZATVORENÉ</p>
+                                </div>
+                            </div>
+
+                            <div class="flex flex-col gap-[var(--small-gap)]">
+                                <p class="bodyText">KONTAKT</p>
+                                <div class="flex flex-col gap-[var(--extra-small-gap)]">
+                                    <p class="pText">0918 523 756</p>
+                                    <p class="pText">034/621 44 00</p>
+                                    <p class="pText">rmtechnikmyjava@gmail.com</p>
+                                    <p class="pText">IČO: 53498615</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- FORM -->
+                <div class="col-start-6 col-span-6">
+
+                </div>
+            </div>
+        </section>
+
+        <!-- FOOTER -->
+        <section class="bg-[var(--secondary-color)] px-4 md:px-16  lg:px-32">
+            <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-8 max-w-[80rem] mx-auto py-10 ">
+                <div class="col-span-4">
+                    <div class="flex flex-col gap-[var(--small-gap)]">
+                        <h2 class="h2Text text-white">RMTechnik</h2>
+                        <p class="pText text-white">
+                            Či už sa staráte o záhradu, pracujete so stromami alebo v lese, radi vám pomôžeme nájsť správne náradie. 
+                            Vyberáme značky, ktorým sami dôverujeme, a poradíme vám tak, aby vám technika vydržala čo najdlhšie.
+                        </p>
+                        <div class="flex gap-[var(--small-gap)]">
+                            <a href="#" class="w-auto h-auto">
+                                <div class="flex rounded-full w-[40px] h-[40px] bg-[var(--accent-primary-color)] text-center items-center justify-center">
+                                    <i class="fa-brands fa-facebook-f text-white"></i>
+                                </div>
+                            </a>
+                            <a href="#" class="w-auto h-auto">
+                                <div class="flex rounded-full w-[40px] h-[40px] bg-[var(--accent-primary-color)] text-center items-center justify-center">
+                                    <i class="fa-brands fa-instagram text-white"></i>
+                                </div>
+                            </a>
+                            <a href="#" class="w-auto h-auto">
+                                <div class="flex rounded-full w-[40px] h-[40px] bg-[var(--accent-primary-color)] text-center items-center justify-center">
+                                    <i class="fa-brands fa-youtube text-white"></i>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-4 lg:col-start-11 lg:col-span-2 flex flex-col gap-[var(--small-gap)]">
+                    <h2 class="bodyText text-white font-[400] textr">NAVŠTÍVTE NÁS</h2>
+                    <div class="flex flex-col gap-[var(--small-gap)]">
+                        <p class="pText text-[var(--accent-primary-color)] font-bold">ADRESA</p>
+                        <p class="pText text-white">Moravská 619, 907 01 Myjava</p>
+                    </div>
+                    <div class="flex flex-col gap-[var(--small-gap)]">
+                        <p class="pText text-[var(--accent-primary-color)] font-bold">KONTAKT</p>
+                        <div class="flex flex-col gap-[var(--extra-small-gap)]">
+                            <p class="pText text-white">0918 523 756</p>
+                            <p class="pText text-white">034/621 44 00</p>
+                            <p class="pText text-white">rmtechnikmyjava@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-4
+                            mt-10
+                            w-full 
+                            h-[0.0625rem] 
+                            bg-[var(--line-color)]">
+                </div>
+                <div class="flex justify-between col-start-1">
+                    <p class="pText text-white py-3">🤎bucak.dev</p>
+                    <p class="pText text-white py-3">RMTechnik all rights reserved 2026</p>
+                </div>
+        </section> 
     </body>
 </html>
